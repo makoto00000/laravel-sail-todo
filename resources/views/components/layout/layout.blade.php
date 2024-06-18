7668f9859b10079f438e5b56a2f1873a7c3e5176
makoto hata <makotohata0429@gmail.com>
2024-06-18 22:04:09 +0900

    add: layout

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>{{ $title ?? Todoアプリ }}</title>
+</head>
+<body class="bg-gray-100">
+    {{ $slot }}
+</body>
+</html>
