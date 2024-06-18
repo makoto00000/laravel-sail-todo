2e29e183c24b33533e47846e6b6f342f7b37fdd4
makoto hata <makotohata0429@gmail.com>
2024-06-18 21:21:29 +0900

    update: routes web for get todolist

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('todolist', \App\Http\Controllers\Todolist\TodolistController::class)->name('todolist');
