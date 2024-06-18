e48844dedb613c936e48928fa9441811bf106057
makoto hata <makotohata0429@gmail.com>
2024-06-19 08:33:32 +0900

    add: routes put delete

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ Route::get('/', function () {
 });
 
 Route::get('todolist', \App\Http\Controllers\Todolist\TodolistController::class)->name('todolist');
+
+Route::put('/checktodo')->name('checktodo');
+Route::delete('/deletetodo')->name('deletetodo');
