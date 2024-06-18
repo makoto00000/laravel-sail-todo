aec46531c3d9b8a00984e7d643b3ae2f73458837
makoto hata <makotohata0429@gmail.com>
2024-06-18 20:41:27 +0900

    update: TodoSeeder

--- database/seeders/TodoSeeder.php
+++ database/seeders/TodoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Todolist;
 
 class TodoSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class TodoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Todolist::factory()->count(30)->create();
     }
 }
